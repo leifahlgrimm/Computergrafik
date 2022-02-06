e216ed80bc9b4f0bbfa4d9df11a232485ab7928d
Leif Ahlgrimm <58110221+leifahlgrimm@users.noreply.github.com>
2022-02-06 20:54:21 +0100

    Update sphere.js

--- cog1/modelData/sphere.js
+++ cog1/modelData/sphere.js
@@ -32,6 +32,27 @@
 define(["exports", "data", "glMatrix"], function(exports, data) {
 	"use strict";
 
+	const init_vertices = [
+		[1.0, 0.0, 0.0], 	// 0
+		[-1.0, 0.0, 0.0], 	// 1
+		[0.0, 1.0, 0.0], 	// 2
+		[0.0, -1.0, 0.0], 	// 3
+		[0.0, 0.0, 1.0], 	// 4
+		[0.0, 0.0, -1.0] 	// 5
+	];
+
+	const init_polygon = [
+		[0, 4, 2],
+		[2, 4, 1],
+		[1, 4, 3],
+		[3, 4, 0],
+		[0, 2, 5],
+		[2, 1, 5],
+		[1, 3, 5],
+		[3, 0, 5]
+	];
+
+
 	/**
 	 * Procedural calculation.
 	 *
@@ -67,15 +88,30 @@ define(["exports", "data", "glMatrix"], function(exports, data) {
 
 		// BEGIN exercise Sphere
 
-		// Starting with octahedron vertices
+		instance.vertices = [
+			[0,1,0],
+			[1,0,0],
+			[0,0,-1],
+			[-1,0,0],
+			[0,0,1],
+			[0,-1,0]
+		]
 
-		// octahedron triangles
+		instance.polygonVertices = [
+			[0,4,1],
+			[0,1,2],
+			[0,2,3],
+			[0,3,4],
+
+			[1,4,5],
+			[2,1,5],
+			[3,2,5],
+			[4,3,5],
+		]
 
 		devide_all.call(instance, recursionDepth);
 
 		// END exercise Sphere
-		
-		devide_all.call(instance, recursionDepth);
 
 		generateTextureCoordinates.call(instance);
 
@@ -128,7 +164,7 @@ define(["exports", "data", "glMatrix"], function(exports, data) {
 				
 
 		// Problem with phi/u: phi=360 and phi=0 are the same point in 3D and also on a tiled texture.
-		// But for faces it is a difference if uv-range is 350¡-360¡ [.9-1]or 350¡-0¡ [.9-0].
+		// But for faces it is a difference if uv-range is 350ï¿½-360ï¿½ [.9-1]or 350ï¿½-0ï¿½ [.9-0].
 		// Thus, insert a check/hack (assuming faces cover only a small part of the texture):
 
 			// Check if u-range should be low or high (left or right in texture),
@@ -140,46 +176,97 @@ define(["exports", "data", "glMatrix"], function(exports, data) {
 	}
 
 	// BEGIN exercise Sphere
+	function center(a, b) {
+		var x = (a[0] + b[0]) / 2;
+		var y = (a[1] + b[1]) / 2;
+		var z = (a[2] + b[2]) / 2;
+		return [x,y,z]
+	}
+
+	function projectPoint(p) {
+		var f = Math.sqrt(1 / (p[0] * p[0] + p[1] * p[1] + p[2] * p[2]));
+		var x = (p[0] * f);
+		var y = (p[1] * f);
+		var z = (p[2] * f);
+		return [ x, y, z]
+	}
 
 	/**
 	 * Recursively divide all triangles.
 	 */
 	function devide_all(recursionDepth, nbRecusions) {
+		var instance = this;
+
 		// nbRecusions is not set from initial call.
 		if(nbRecusions == undefined) {
 			nbRecusions = 0;
 		}
 		// Stop criterion.
+		if(nbRecusions == recursionDepth) {
+			return;
+		}
+		console.log("nbRecusions: "+nbRecusions);
 
-		//console.log("nbRecusions: "+nbRecusions);
-		// Assemble divided polygons in an new array.
+		// Assemble divided polygons in an new array.#
+		var newPoly = []
+		var polycount = instance.polygonVertices.length
 
-			// Indices of the last three new vertices.
+		// Indices of the last three new vertices.
+		for(var p=0; p < polycount; p++) {
+			// Calculate new vertex in the middle of edge.
+			var p1 = (center(instance.vertices[instance.polygonVertices[p][0]],
+				instance.vertices[instance.polygonVertices[p][1]]));
+			var p2 = (center(instance.vertices[instance.polygonVertices[p][1]],
+				instance.vertices[instance.polygonVertices[p][2]]));
+			var p3 = (center(instance.vertices[instance.polygonVertices[p][2]],
+				instance.vertices[instance.polygonVertices[p][0]]));
 
-				// Calculate new vertex in the middle of edge.
+			var newPoints = []
+			newPoints.push(projectPoint(p1));
+			newPoints.push(projectPoint(p2));
+			newPoints.push(projectPoint(p3));
 
-				// Check if the new vertex exists already.
-				// This happens because edges always belong to two triangles.
+			// Check if the new vertex exists already.
+			// This happens because edges always belong to two triangles.
+			var ref = []
+			for (var j = 0; j < 3; j++) {
+				let existingVertexId = -1
+				for (let k = 0; k < instance.vertices.length; k++) {
+					let current = instance.vertices[k]
+					if (current[0] === newPoints[j][0] && current[1] === newPoints[j][1] && current[2] === newPoints[j][2]) {
+						existingVertexId = k;
+						break;
+					}
+				}
 
-					// Remember index of new vertex.
-
-					//console.log("Calculate new vertex "+v+"->"+newIndex[v]+" : "+vertices[p[v]]+" + "+ vertices[p[next]]+" = "+ newVertex);
-
+				if (existingVertexId != -1) {
 					// Use the existing vertex for the new polygon.
+					ref[j] = existingVertexId
+				} else {
+					// Remember index of new vertex.
+					instance.vertices.push(newPoints[j])
+					ref[j] = instance.vertices.length - 1
 
-					//console.log("New vertex exists "+v+"->"+newIndex[v]+" : "+this.vertices[p[v]]+" + "+ this.vertices[p[next]]+" = "+ newVertex);
+				}
+			}
 
 			// Assemble new polygons.
 			// Assure mathematical positive order to keep normals pointing outwards.
 			// Triangle in the center.
-
+			newPoly.push([ref[0], ref[1], ref[2]])
 			// Triangle in the corners.
+			newPoly.push([ref[0], ref[2], instance.polygonVertices[p][0]])
+			newPoly.push([ref[1], ref[0], instance.polygonVertices[p][1]])
+			newPoly.push([ref[2], ref[1], instance.polygonVertices[p][2]])
 
-				//console.log("Assemble new polygons "+v+" : "+p[v]+" , "+ newIndex[nextButOne]+" , "+ newIndex[v]);
-
+			//console.log("Assemble new polygons "+v+" : "+p[v]+" , "+ newIndex[nextButOne]+" , "+ newIndex[v]);
+		}
 		// Swap result.
-
+		instance.polygonVertices = newPoly;
 		// Recursion.
+		nbRecusions = nbRecusions +1;
+
+		devide_all.call(instance, recursionDepth, nbRecusions);
 
 	}
 	
