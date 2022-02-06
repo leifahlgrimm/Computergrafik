f69c95cd842aa5fe5f0140e108b1d63ed4dd0f83
Leif Ahlgrimm <58110221+leifahlgrimm@users.noreply.github.com>
2022-02-06 20:52:37 +0100

    Update createScene.js

--- cog1/createScene.js
+++ cog1/createScene.js
@@ -16,56 +16,55 @@ function(exports, scenegraph, animation) {
 	 *
 	 */
 	function init() {
+		// BEGIN exercise Scenegraph
 
+
+		var sphere = scenegraph.createNodeWithModel("sphere 1", "sphere", {recursionDepth: 3, scale: 300});
 		// var line = scenegraph.createNodeWithModel("line", "line", {scale:200});
 
 		// var insideOutPolyNode = scenegraph.createNodeWithModel("insideOutPoly", "insideOutPoly");
 
-		var dirtyTeapotNode = scenegraph.createNodeWithModel("dirtyTeapot", "teapot_dirty", {color:8});
+		// var teapotNode = scenegraph.createNodeWithModel("teapot", "teapot", {color:0, scale:40});
+		// var dirtyTeapotNode = scenegraph.createNodeWithModel("dirtyTeapot", "teapot_dirty", {color:8});
 
-		var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:300});
-		cubeNode.rotateTo([1.6, -3.87, 0]);
+		// var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:300});
+		// cubeNode.rotateTo([1.6, -3.87, 0]);
+
+		// var witcher = scenegraph.createNodeWithModel("witcher", "witcher", {scale: 20});
+		// witcher.rotateTo([1.6, 0, 0]);
 
 		// BEGIN exercise myModel
 		// var church = scenegraph.createNodeWithModel("church", "church", {scale:60});
 		// church.rotateTo([0.3, -3.87, 0]);
 		// END exercise myModel
-		
-		return;
+
 
 		//var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:100, textureURL:"brickWall.jpg"});		
-		var cubeNode1 = scenegraph.createNodeWithModel("cube 1", "cube", {scale:70, textureURL:"stoneWall.jpg"});
-		cubeNode1.translate([50,200,0]);
+		// var cubeNode1 = scenegraph.createNodeWithModel("cube 1", "cube", {scale:70, textureURL:"stoneWall.jpg"});
+		// cubeNode1.translate([50,200,0]);
 		//cubeNode1.rotate([1,1,1]);
-		var cubeNode2 = scenegraph.createNodeWithModel("cube 2", "cube", {scale:50, textureURL:"uvTest.jpg"});
-		cubeNode2.translate([-100,-400,0]);
+		//var cubeNode2 = scenegraph.createNodeWithModel("cube 2", "cube", {scale:50, textureURL:"uvTest.jpg"});
+		//cubeNode2.translate([-100,-400,0]);
 		//cubeNode2.rotate([-1,-1,-1]);
-		var cubeNode3 = scenegraph.createNodeWithModel("cube procedural texture", "cube", {scale:50, textureURL:"sampleProceduralRGB"});
-		var cubeNode4 = scenegraph.createNodeWithModel("cube six faces texture", "cube", {scale:200, textureURL:"OrbitCube.gif", sixFacesTexture:true});
-		var cubeNode5 = scenegraph.createNodeWithModel("cube 3x3 texture", "cube", {scale:50, textureURL:"cubeColor.png", sixFacesTexture:true});
-		var cubeNode6 = scenegraph.createNodeWithModel("cube Escher texture", "cube", {scale:200, textureURL:"EscherCubeFish.gif", sixFacesTexture:true});
+		//var cubeNode3 = scenegraph.createNodeWithModel("cube procedural texture", "cube", {scale:50, textureURL:"sampleProceduralRGB"});
+		//var cubeNode4 = scenegraph.createNodeWithModel("cube six faces texture", "cube", {scale:200, textureURL:"OrbitCube.gif", sixFacesTexture:true});
+		//var cubeNode5 = scenegraph.createNodeWithModel("cube 3x3 texture", "cube", {scale:50, textureURL:"cubeColor.png", sixFacesTexture:true});
+		//var cubeNode6 = scenegraph.createNodeWithModel("cube Escher texture", "cube", {scale:200, textureURL:"EscherCubeFish.gif", sixFacesTexture:true});
 
-		var diamondNode = scenegraph.createNodeWithModel("diamond", "diamond");
+		//var diamondNode = scenegraph.createNodeWithModel("diamond", "diamond");
 
-		var torusNode = scenegraph.createNodeWithModel("torus", "torus");
-		var torusNode1 = scenegraph.createNodeWithModel("torus 13", "torus", {r2:50,n2:13,color:8});
+		//var torusNode = scenegraph.createNodeWithModel("torus", "torus");
+		//var torusNode1 = scenegraph.createNodeWithModel("torus 13", "torus", {r2:50,n2:13,color:8});
 
-		var teapotNode = scenegraph.createNodeWithModel("teapot", "teapot", {color:0, scale:40});
+		//var teapotNode = scenegraph.createNodeWithModel("teapot", "teapot", {color:0, scale:40});
 
 		//teapotNode.rotate([1,1,1]);
 		
-		var waltheadNode = scenegraph.createNodeWithModel("walthead", "walthead", {color:8});
+		//var waltheadNode = scenegraph.createNodeWithModel("walthead", "walthead", {color:8});
 		
-		var plainNode1 = scenegraph.createNodeWithModel("plain", "plain", {scale:200, color:9, textureURL:"land_ocean_ice_2048.jpg"});		
+		//var plainNode1 = scenegraph.createNodeWithModel("plain", "plain", {scale:200, color:9, textureURL:"land_ocean_ice_2048.jpg"});
 
-		var emptyNode1 = scenegraph.createNodeWithModel("empty", "empty");		
-
-
-        // BEGIN exercise Scenegraph		
-		
-		// Set parent-child relationships for scene-graph nodes.
-
-        // END exercise Scenegraph		
+		//var emptyNode1 = scenegraph.createNodeWithModel("empty", "empty");
         
         // Assign animations.
         // animation.assign(cubeNode, "move");
@@ -86,21 +85,19 @@ function(exports, scenegraph, animation) {
 		// Set visibility of nodes (hide: set to false).
 		// Comment out what you want to see as the default is visible.
         // cubeNode.setVisible(false);
-        cubeNode1.setVisible(false);
+        /*cubeNode1.setVisible(false);
         cubeNode2.setVisible(false);
         cubeNode3.setVisible(false);
         cubeNode4.setVisible(false);
         cubeNode5.setVisible(false);
         cubeNode6.setVisible(false);
-        insideOutPolyNode.setVisible(false);
         diamondNode.setVisible(false);
         torusNode.setVisible(false);
         torusNode1.setVisible(false);
         teapotNode.setVisible(false);
-        dirtyTeapotNode.setVisible(false);
         waltheadNode.setVisible(false);
         plainNode1.setVisible(false);
-        emptyNode1.setVisible(false);
+        emptyNode1.setVisible(false);*/
 
         
 		// Set the initially interactive node [by name].
